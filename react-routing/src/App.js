369f69500badcd685acc8471f43802a2243e3d20
rishi05051997 <vrushabhdhatral10@gmail.com>
2021-06-22 07:37:33 +0530

    added navlinks with styling

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -1,18 +1,28 @@
-import{ Route }from "react-router-dom"
+import { Route } from "react-router-dom"
 import './App.css';
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import MainHeader from "./Components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/welcome">
-        < Welcome />
-      </Route>
+      < MainHeader />
+      <main>
+        <Route path="/welcome">
+          < Welcome />
+        </Route>
 
-      <Route path="/products">
-        < Products />
-      </Route>
+        <Route path="/products">
+          < Products />
+        </Route>
+
+        <Route path="/product-detail/:productId">
+          < ProductDetail />
+        </Route>
+      </main>
+
     </div>
   );
 }
@@ -22,3 +32,4 @@ export default App;
 
 // our-domain.com/welcome => Welcome Component
 // our-domain.com/products => Products Component
+// our-domain.com/product-details/<any-value> => Product Details Component
---
+++ react-routing/src/pages/ProductDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProductDetail() {
+    return (
+        <section>
+            <h1>Products</h1>
+        </section>
+    )
+}
